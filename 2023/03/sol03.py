3cf5e82ac32272d29c05ab585740b1036d07317f
freitag <niels.freitag@rwth-aachen.de>
2023-12-03 09:20:13 +0100

    day 03

---
+++ 2023/03/sol03.py
@@ -0,0 +1,88 @@
+import pathlib
+import numpy as np
+import string
+
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+def parse(puzzle_input):
+    """Parse input."""
+    all_punctuation = string.punctuation
+    all_punctuation = all_punctuation.replace(".","#")
+    for char in all_punctuation:
+        # for char in (string.punctuation).replace(".","#"))
+        puzzle_input = puzzle_input.replace(char, "#")
+    width = len(puzzle_input.splitlines()[-1])
+    height = len(puzzle_input.splitlines())
+    puzzle_input = puzzle_input.replace("\n", "")
+    return string_to_field(puzzle_input, width, height)
+
+"""
+def filter (data):
+    filter_array = np.array([[1,1,1],[1,0,1],[1,1,1]])
+    result = np.zeros(data.shape)
+    for i in range(data):
+        field = np.pad(data, 1, mode='constant', constant_values='.')
+        for j in range(len(data[0])):
+            if data[i][j].isDigit():
+                result[i][j] = np.multiply(field[i-1:i+1][j-1:j+1]=='#', filter_array)
+        field = np.where(field!=0, 1, 0)
+"""
+
+def filter (data):
+    result =[]
+    field = np.pad(data, 1, mode='constant', constant_values='.')
+    for i in range(len(data)):
+        j = 0
+        while j < len(data[0]):
+            if np.char.isdigit(data[i,j]):
+                len_number = 1
+                while(str(field[i+1, j+len_number+1]).isdigit()):
+                    len_number += 1
+                if '#' in field[i:i+3, j:j+len_number+2]:
+                    whole_number = ""
+                    for k in range(len_number):
+                        whole_number = whole_number + data[i,j+k]
+                    print(whole_number)
+                    result.append(int(whole_number))
+                j += len_number
+            j += 1
+    return result              
+
+def part1(data):
+    """Solve part 1."""
+    return sum(filter(data))
+    
+
+
+def part2(data):
+    """Solve part 2."""
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+#This file contains various functions that provide functionalities useable in the event tasks
+
+##Functions for input formatting--------------------------------------------------------------
+#
+
+def string_to_field(string_in, height, width, index_first=0, index_last=None):
+    if not index_last: index_last=len(string_in)
+    text=string_in[index_first:index_last].ljust(height*width, '0')
+    arr=np.asarray([char for char in text])
+    return arr.reshape(height,width)
+
+
+#---------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    puzzle_input = (PUZZLE_DIR / "input.txt").read_text().strip()
+    solutions = solve(puzzle_input)
+    print("\n".join(str(solution) for solution in solutions))
---
+++ 2023/03/test_03.py
@@ -0,0 +1,35 @@
+import pathlib
+import pytest
+import sol03 as sol
+import numpy as np
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return sol.parse(puzzle_input)
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return sol.parse(puzzle_input)
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    sollte_so_aussehen =np.array([['4','6','7','.','.','1','1','4','.','.'], ['.','.','.','#','.','.','.','.','.','.'], ['.', '.', '3', '5','.','.','6','3','3','.'], ['.', '.', '.', '.', '.', '.', '#', '.', '.', '.'], ['6','1','7','#','.','.','.','.','.','.'], ['.','.', '.', '.', '.', '#', '.', '5', '8', '.'], ['.', '.', '5', '9', '2', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '7', '5', '5', '.'], ['.', '.', '.', '#', '.', '#', '.', '.', '.', '.'], ['.', '6', '6', '4', '.', '5', '9', '8', '.', '.']])
+    assert (example1 == sollte_so_aussehen).all()
+
+def test_part1_example1(example1):
+    """Test part 1 on example1 input."""
+    assert sol.part1(example1) == 4361
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example1(example1):
+    """Test part 2 on example1 input."""
+    assert sol.part2(example1) == ...
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example2 input."""
+    assert sol.part2(example2) == ...
