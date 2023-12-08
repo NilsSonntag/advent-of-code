1eaa48b922510992f5559eea0166eb49aed0f5ec
freitag <niels.freitag@rwth-aachen.de>
2023-12-08 09:26:31 +0100

    Fix part2 function in sol08.py

--- 2023/08/sol08.py
+++ 2023/08/sol08.py
@@ -48,88 +48,6 @@ def nodes_ending_on_A (transitions: Any) -> list:
         if node[2] == "A":
             nodes_ending_on_A.append(node)
     return nodes_ending_on_A
-"""
-def part2(data: Any) -> int:
-    left_rights, transitions = data
-    steps = 0
-    left_rights = deque(left_rights)
-    positions = set(nodes_ending_on_A(transitions))
-    found = False
-    while not found:
-        found = True
-        lr = left_rights[0]
-        updated_positions = set()
-        for position in positions:
-            updated_position = transitions[position][0] if lr == "L" else transitions[position][1]
-            if updated_position[2] != "Z":
-                found = False
-                updated_positions.add(updated_position)
-        if not found and steps > len(left_rights):
-            mu, lam = find_cycle(position, cycle(left_rights), transitions)
-            if lam != 0:
-                cycle_repeats = (len(left_rights) - mu) // lam
-                steps += cycle_repeats * lam
-                left_rights.rotate(cycle_repeats * lam)
-        else:
-            steps += 1
-            left_rights.rotate(1)
-        positions = updated_positions
-    return steps
-
-def part2(data: Any) -> int:
-    left_rights, transitions = data
-    steps = 0
-    left_rights = deque(left_rights)
-    positions = set(nodes_ending_on_A(transitions))
-    found = False
-    while not found:
-        found = True
-        lr = left_rights[0]
-        updated_positions = set()
-        for position in positions:
-            if lr == "L":
-                updated_position = transitions[position][0]
-            else:
-                updated_position = transitions[position][1]
-            if updated_position[2] != "Z":
-                found = False
-            updated_positions.add(updated_position)
-        steps += 1
-        left_rights.rotate(1)
-        positions = updated_positions
-    return steps
-
-def find_cycle(position, left_rights, transitions):
-    # Define a helper function that applies the transition
-    def f(x):
-        direction = next(left_rights)
-        return transitions[x][0 if direction == "L" else 1]
-
-    # Phase 1: Find a repetition x_mu = x_2mu.
-    tortoise = f(position)
-    hare = f(f(position))
-    while tortoise != hare:
-        tortoise = f(tortoise)
-        hare = f(f(hare))
-
-    # Phase 2: Find the position of the first repetition of length lambda.
-    mu = 0
-    hare = position
-    while tortoise != hare:
-        tortoise = f(tortoise)
-        hare = f(hare)
-        mu += 1
-
-    # Phase 3: Find the length of the shortest cycle starting from x_mu.
-    lam = 1
-    hare = f(tortoise)
-    while tortoise != hare:
-        hare = f(hare)
-        lam += 1
-
-    return mu, lam
-"""
-
 
 def part2 (data: Any) -> int:
     left_rights, transitions = data
@@ -169,8 +87,7 @@ def lcm(numbers):
 def solve(puzzle_input: str) -> Tuple[int, int]:
     """Solve the puzzle for the given input and return the solutions for part 1 and part 2."""
     data = parse(puzzle_input)
-    solution1 = 0
-    #solution1 = part1(data)
+    solution1 = part1(data)
     start=time.time()
     solution2 = part2(data)
     print("Time: " + str(time.time()-start))
