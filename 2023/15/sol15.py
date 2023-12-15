49f15022168af4f309b50288443bd222019e1cc3
Jonathan <lyssy.jonathan@gmail.com>
2023-12-15 07:09:08 +0100

    sol15.2

--- 2023/15/sol15.py
+++ 2023/15/sol15.py
@@ -7,23 +7,67 @@ def parse(puzzle_input: str) -> list[str]:
     """Parse the puzzle input and return a data structure."""
     return puzzle_input.split(",")
 
-
+def hash_algorithm(data: str) -> int:
+    """Hash algorithm for the given data."""
+    current_value = 0
+    for letter in data:
+        current_value += ord(letter)
+        current_value *= 17
+        current_value %= 256
+    return current_value
 
 def part1(data: list[str]) -> int:
     """Solve part 1 of the puzzle for the given data and return the solution."""
     sum_of_results = 0
     for item in data:
-        current_value = 0
-        for letter in item:
-            current_value += ord(letter)
-            current_value *= 17
-            current_value %= 256
-        sum_of_results += current_value
+        sum_of_results += hash_algorithm(item)
     return sum_of_results
 
+def split_string(string: str) -> list[str]:
+    """Split the given string into a list of strings. Seperated by the caracter '-' or '='"""
+    return string.split("-") if "-" in string else string.split("=")
 
-def part2(data: Any) -> int:
+def part2(data: list[str]) -> int:
     """Solve part 2 of the puzzle for the given data and return the solution."""
+    boxes = {}
+    for i in range(256):
+        boxes[i] = []
+    
+    for lens in data:
+        lens = split_string(lens)
+        label = lens[0]
+        box_index = hash_algorithm(label)
+        
+        label_index = None
+        for i in range(len(boxes[box_index])):
+            if label in boxes[box_index][i]:
+                label_index = i
+                break
+        
+        if lens[1] is not "":
+            is_contained = False
+            for lenses in boxes[box_index]:
+                if label == lenses[0]:
+                    boxes[box_index][label_index] = lens
+                    is_contained = True
+                    break
+            if not is_contained:
+                boxes[box_index].append(lens)
+        elif label_index is not None:
+            del boxes[box_index][label_index]
+    
+    print(boxes)
+    
+    sum_of_powers=0
+    for box in boxes:
+        index_of_box = box + 1
+        for lens in boxes[box]:
+            index_of_slot = boxes[box].index(lens) + 1
+            focal_length = int(boxes[box][index_of_slot - 1][1])
+            sum_of_powers += focal_length * index_of_box * index_of_slot
+    
+    return sum_of_powers
+
 
 def solve(puzzle_input: str) -> Tuple[int, int]:
     """Solve the puzzle for the given input and return the solutions for part 1 and part 2."""
