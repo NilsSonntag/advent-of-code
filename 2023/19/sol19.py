27c551fe29de480fd97de8034be891517e2ac5bc
Jonathan <lyssy.jonathan@gmail.com>
2023-12-19 10:12:09 +0100

    sol 19.1

--- 2023/19/sol19.py
+++ 2023/19/sol19.py
@@ -22,9 +22,46 @@ def parse(puzzle_input: str) -> Any:
         parts.append(line)
     return (dict_commands, parts)
 
+end_states = {"A","R"}
+attributes = {"x":0,"m":1,"a":2,"s":3}
+
+def check_if_command_applies(command : str,part) -> str:
+    """Check if the command applies to the part and return the next position for that part."""
+    attribute_to_check,comparison,value_to_check,successor = command[0],command[1],int(command[2]),command[-1]
+    if comparison == "<":
+        if part[attributes[attribute_to_check]]<value_to_check:
+            return successor
+    elif comparison == ">":
+        if part[attributes[attribute_to_check]]>value_to_check:
+            return successor
+    return "not_fiting"
+
+def reparse_command(command: Any) -> Any:
+    command = command.split(":")
+    check,successor = command[0],command[1]
+    attribute_to_check,comparison,value_to_check = check[0],check[1],check[2:]
+    return [attribute_to_check,comparison,value_to_check,successor]
+
+
 def part1(data: Any) -> int:
     """Solve part 1 of the puzzle for the given data and return the solution."""
-    
+    commands, parts = data
+    ans = 0
+    for part in parts:
+        position = "in"
+        while position not in end_states:
+            for command in commands[position]:
+                if command in end_states or command in commands.keys():
+                    position = command
+                    break
+                command_list = reparse_command(command)
+                result = check_if_command_applies(command_list,part)
+                if result != "not_fiting":
+                    position = result
+                    break
+        print(position)
+        ans += sum(part) if position == "A" else 0
+    return ans
 
 def part2(data: Any) -> int:
     """Solve part 2 of the puzzle for the given data and return the solution."""
